6ed0c34d42cf11854a919520d328a7a42651a586
phoenixredwolf <b.vogtman@outlook.com>
2023-09-06 21:58:26 -0400

    Initial commit

---
+++ site/src/jsMain/kotlin/com/phoenixredwolf/prodweb/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package com.phoenixredwolf.prodweb.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
